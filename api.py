3d6790f5447083745b75ba6a85e9b81a1f3789bf
Anton Pleshivtsev <anton@emby.ru>
2015-08-31 23:21:44 +0300

    introduce simple plugin api

---
+++ api.py
@@ -0,0 +1,28 @@
+from bottle import route, run, template
+from os import listdir
+
+METHODS = {}
+FUNC_LIST = []
+
+for lang in listdir('src'):
+    for func in listdir("src/" + lang):
+        func_name, _ = func.split('.')
+        name = lang + "/" + func_name
+
+        FUNC_LIST.append(name)
+        METHODS[name] = open("src/" + lang + "/" + func).read()
+
+
+@route('/')
+def index():
+    return {'functions': len(FUNC_LIST)}
+
+@route('/list')
+def index():
+    return {'functions': FUNC_LIST}
+
+@route('/func/<lang>/<method>')
+def name(lang, method):
+    return {'func': METHODS[lang + '/' + method]}
+
+run(host='localhost', port=8122)
